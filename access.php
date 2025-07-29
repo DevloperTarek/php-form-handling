90c4d26d13cee28b416dd803b0a9c4f60bfba002
Tarek Hossain <devlopertarek433@gmail.com>
2025-07-29 15:46:12 +0600

    php access file have store all data successfull

--- access.php
+++ access.php
@@ -22,3 +22,30 @@
     <h2 class="success-msg">Registration Successfully</h2>
 </body>
 </html>
+
+<?php
+        if(isset($_FILES['file'])){
+            $file = $_FILES['file'];
+            //File Related Data
+            $filename = $file['name'];
+            $temp_path = $file['tmp_name'];
+            $size = $file['size'];
+            $error = $file['error'];
+
+            $upload_folder = "upload/";
+            $destination = $upload_folder . basename($filename);
+
+            //File move to Server
+            if($error === 0){
+                if(move_uploaded_file($temp_path,$destination)){
+                    echo "File Uploaded successfully";
+                }else{
+                    echo "File Uploading Have a Serious Issue for not Uploading";
+                }
+            }else{
+                echo "File Uploading Have an Error Throw";
+            }
+        }else{
+            echo "Not selected any type of file";
+        }
+?>
