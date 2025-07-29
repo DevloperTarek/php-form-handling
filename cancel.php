dd6eee8a61f49bad2b5698458dec9f0d0b116060
Tarek Hossain <devlopertarek433@gmail.com>
2025-07-29 13:10:55 +0600

    myDBPDO

--- cancel.php
+++ cancel.php
@@ -1,1 +1,14 @@
 
+<?php
+    // Server Connection
+    $server = 'localhost';
+    $username = "root";
+    $password = "";
+    $database = "registrationdb";
+
+    try{
+        $conn = new PDO("mysql:host=$server",$username,$password,$database);
+        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        $sql = "CREATE DATABASE myDBPDO";
+    }
+?>
